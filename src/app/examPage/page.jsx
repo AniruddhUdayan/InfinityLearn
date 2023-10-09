9a5f2bc8621d10a2f55a7dea72ba297225ff7940
Satyam Sagar <satyamsagar1711@gmail.com>
2023-10-09 13:27:56 +0530

    added header footer, and download sections

--- src/app/examPage/page.jsx
+++ src/app/examPage/page.jsx
@@ -1,10 +1,19 @@
 "use client"
 // import LandingExam from "../../components/LandingExam"
+import LandingExam from '@/components/examPage/LandingExam'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import Download from '@/components/examPage/Download'
+import School from '@/components/examPage/School'
 
 const ExamPage = () => {
     return (
         <>
-            {/* <LandingExam /> */}
+            <Header />
+            <LandingExam />
+            <School />
+            <Download />
+            <Footer /> 
         </>
     )
 }
