557de4359b6199045a9fc588cb1c9a9a75407d0d
masterbhuvan1 <bhuvanjain212003@gmail.com>
2023-10-09 15:17:41 +0530

    xyz

--- src/components/homePage/firstSection.jsx
+++ src/components/homePage/firstSection.jsx
@@ -1,11 +1,39 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import WordSlider from "./wordSlider";
+
 import { useDispatch, useSelector } from "react-redux";
 import { showOverlayMode } from "@/store/mobVeriSlice";
 import { storePhoneNumber } from "@/store/mobVeriSlice";
 import LoginPage from "../loginPage";
+const words = ["learning", "academic"];
+const duration = 2000; // Duration in milliseconds for each word
+
+function WordSlider() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [animating, setAnimating] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAnimating(false);
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
+        setAnimating(true);
+      }, 1000); // waits for the animation to complete
+    }, duration + 1000); // adds the animation duration to the waiting duration
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="slider-container z-0 h-24 max-md:h-12 max-md:pl-3 max-md:text-5xl text-yellow-400 flex items-center justify-start text-7xl font-bold">
+      <div className={animating ? "word-entering z-0" : " z-0 word-exiting"}>
+        {words[currentIndex]}
+      </div>
+    </div>
+  );
+}
+
 function Trial() {
   return (
     <div className="flex mt-10 max-lg:w-full mb-6 max-lg:mx-10 max-lg:h-24 justify-evenly text-[#007BFF] p-6  text-center  font-bold text-base gap-3 mx-auto flex-row items-center h-20 bg-white px-4 rounded-2xl">
