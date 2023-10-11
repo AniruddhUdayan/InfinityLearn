941a00eeed103dabdf725ab3da0dfb7bebb5470f
InfiniyleanWebsite <c_venkatkiran.t@infinitylearn.com>
2023-10-11 18:56:45 +0530

    popup without mobile

--- src/components/loginPage/mobileVerify/tick.jsx
+++ src/components/loginPage/mobileVerify/tick.jsx
@@ -5,13 +5,13 @@ function Tick() {
   return (
     <div>
       <svg
-        class="checkmark"
+        className="checkmark"
         xmlns="http://www.w3.org/200/svg"
         viewBox="0 0 52 52"
       >
-        <circle class="verified-badge" cx="26" cy="26" r="25" fill="none" />
+        <circle className="verified-badge" cx="26" cy="26" r="25" fill="none" />
         <path
-          class="checkmark__check"
+          className="checkmark__check"
           fill="none"
           d="M14.1 27.2l7.1 7.2 16.7-16.8"
         />
