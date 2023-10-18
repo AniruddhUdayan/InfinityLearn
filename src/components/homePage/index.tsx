ae0c534cf2c07c83ca659df81a240496133f7d6a
AniruddhUdayan <aniruddhudayan34@gmail.com>
2023-10-18 14:43:30 +0530

    err

--- src/components/homePage/index.tsx
+++ src/components/homePage/index.tsx
@@ -1,15 +1,9 @@
 "use client";
 import React, { useEffect } from "react";
 import Header from "../header.jsx";
-<<<<<<< HEAD
-import Footer from "./footer.jsx";
-import FirstSection from "./firstSection.jsx";
-import SecondSection from "./secondSection.jsx";
-=======
 import Footer from "../homePage/footer.jsx";
 import FirstSection from "./firstSection/firstSection.jsx";
 import SecondSection from "./secondSection/secondSection.jsx";
->>>>>>> db7ecd6dc6b500faff30f2746dcc4dbf1b0919d8
 import ThirdSection from "./thirdSection.jsx";
 import FourthSection from "./fourthSection.jsx";
 import FifthSection from "./fifthSection.jsx";
@@ -23,30 +17,6 @@ import SelectGradePopup from "../common/SelectGradePopup.jsx";
 import BookSessionPopup from "../bookSessionPopup";
 import PackageSubscription from "../PackageSubscription/index.jsx";
 const HomePage = () => {
-<<<<<<< HEAD
-    useEffect(() => {
-        analytics.page() 
-      }, [])
-    return (
-        <div>
-            <Header />
-            <FirstSection />
-            <SecondSection />
-            <ThirdSection />
-            <FourthSection />
-            <FifthSection />
-            {/* <Download /> */}
-            <SixthSection />
-            <SeventhSection />
-            <Footer/>
-            <LoginPopup />
-            <SelectGradePopup />
-            <BookSessionPopup />
-        </div>
-    )
-}
-
-=======
   useEffect(() => {
     analytics.page();
   }, []);
@@ -69,6 +39,5 @@ const HomePage = () => {
     </div>
   );
 };
->>>>>>> db7ecd6dc6b500faff30f2746dcc4dbf1b0919d8
 
 export default HomePage;
